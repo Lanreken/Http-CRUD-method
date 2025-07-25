79bdc86cefb87547c712d6a3aeda77256d1d19f0
Lanreken <adelugbaolanrewaju@gmail.com>
2025-07-25 15:51:46 +0100

    added the gitchecking file

---
+++ gitchecking.js
@@ -0,0 +1,7 @@
+const students = [
+  { name: "Favour", age: 20, stack: "backend" },
+  { name: "Paul", age: 22, stack: "frontend" },
+  { name: "Joshua", age: 21, stack: "fullstack" },
+];
+
+console.log(students);
